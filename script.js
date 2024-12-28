54ff21df93f87698ad582b663fa236aac5f580b8
Balamiayush <aryanbalami54@gmail.com>
2024-12-28 14:09:54 +0545

    res

--- script.js
+++ script.js
@@ -11,37 +11,52 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
-let tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".hero1",
-    start: "0% 50%",
-    end: "100% 50%",
-    scrub: 1,
-    // markers: true
-  },
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 1200px)", () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".hero1",
+      start: "0% 50%",
+      end: "100% 50%",
+      scrub: 1,
+    },
+  });
+  tl.to(".img1", { x: -500, rotate: -30, ease: "power3.inOut" }, "a")
+    .to(".img2", { x: -500, rotate: -30, ease: "power3.inOut" }, "a")
+    .to(".img3", { x: 500, rotate: 30, ease: "power3.inOut" }, "a")
+    .to(".img4", { x: 500, rotate: 30, ease: "power3.inOut" }, "a")
+    .to(".hero1 h1", { opacity: 1, y: -100, duration: 1, ease: "power3.inOut" }, "a");
 });
-tl.to(".img1",{
-    x:-500,
-    rotate:-30,
-    ease: "power3.inOut"
-},'a')
-tl.to(".img2",{
-    x:-500,
-    rotate:-30,
-    ease: "power3.inOut"
-},'a')
-tl.to(".img3",{
-    x:500,
-    rotate:30,
-    ease: "power3.inOut"
-},'a')
-tl.to(".img4",{
-    x:500,
-    rotate:30,
-    ease: "power3.inOut"
-},'a')
-tl.to(".hero1 h1",{
-    opacity: 1,
-    duration: 1,
-    ease: "power3.inOut"
-},'a')
+
+mm.add("(min-width: 800px) and (max-width: 1199px)", () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".hero1",
+      start: "0% 50%",
+      end: "100% 50%",
+      scrub: 1,
+    },
+  });
+  tl.to(".img1", { x: -300, rotate: -20, ease: "power3.inOut" }, "a")
+    .to(".img2", { x: -300, rotate: -20, ease: "power3.inOut" }, "a")
+    .to(".img3", { x: 300, rotate: 20, ease: "power3.inOut" }, "a")
+    .to(".img4", { x: 300, rotate: 20, ease: "power3.inOut" }, "a")
+    .to(".hero1 h1", { opacity: 1, y: -80, duration: 1, ease: "power3.inOut" }, "a");
+});
+
+mm.add("(max-width: 799px)", () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".hero1",
+      start: "0% 50%",
+      end: "100% 50%",
+      scrub: 1,
+    },
+  });
+  tl.to(".img1", { x: -100, rotate: -10, ease: "power3.inOut" }, "a")
+    .to(".img2", { x: -100, rotate: -10, ease: "power3.inOut" }, "a")
+    .to(".img3", { x: 100, rotate: 10, ease: "power3.inOut" }, "a")
+    .to(".img4", { x: 100, rotate: 10, ease: "power3.inOut" }, "a")
+    .to(".hero1 h1", { opacity: 1, y: -50, duration: 1, ease: "power3.inOut" }, "a");
+});
